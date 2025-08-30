ed7a910f203331f8ea6e21e3fdd82e68d7253059
taholme <theodor@fjellstua.net>
2025-08-30 00:05:42 +0000

    patch: fixed navigation button on homepage

--- navigation.js
+++ navigation.js
@@ -1,5 +1,5 @@
 // Navigation
 const backButtonEl = document.getElementById("back-button");
-backButtonEl.textContent = window.location.pathname.endsWith("index.html")
+backButtonEl.textContent = window.location.pathname.endsWith("index.html") || window.location.pathname === "/"
 	? ""
 	: "Go Back";
